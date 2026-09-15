751b70b5f28bd853544c15d877d835c22957f5dc
cataaa1 <catalina@marquevich.com>
2026-09-15 15:03:16 -0300

    paso 6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,14 +6,42 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
+const Statistics = ({good,neutral,bad}) => {
+  if (good == 0 && neutral == 0 && bad == 0)
+    return <h3> No feedback given</h3>
+
+  return (
+    <>
+      <h2>Statistics</h2>
+      <table>
+        <tbody>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={good + neutral + bad} />
+      <StatisticLine text="averege" value={(good - bad) / (good + neutral + bad)} />
+      <StatisticLine text="positive" value={good / (good + neutral + bad) * 100 + "%"} />
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+const StatisticLine = ({text,value}) => {
+  return ( 
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const sum = good + neutral + bad
-  const average = sum === 0 ? 0 : (good - bad) / sum
-  const positive = sum == 0 ? 0 : good / sum * 100
+ 
 
   const handleGoodClick = () => setGood(good + 1)
   const handleNeutralClick = () => setNeutral(neutral + 1)
@@ -26,13 +54,8 @@ const App = () => {
       <Button handleClick={handleNeutralClick} text='neutral' />
       <Button handleClick={handleBadClick} text='bad' />
 
-      <h2>Statistics</h2>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>All: {sum}</p>
-      <p>Average: {average}</p>
-      <p>Positive: {positive} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+     
    </div>
   )
 }
