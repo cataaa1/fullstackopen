40e2e5be4cc725b1af8bc2e6adc1feaf229f23f5
cataaa1 <catalina@marquevich.com>
2026-09-15 14:37:28 -0300

    step 1 & 2

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const sum = good + neutral + bad
+  const average = sum === 0 ? 0 : (good - bad) / sum
+  const positive = sum == 0 ? 0 : good / sum * 100
+
+  const handleGoodClick = () => setGood(good + 1)
+  const handleNeutralClick = () => setNeutral(neutral + 1)
+  const handleBadClick = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <h2>Give Feedback</h2>
+      <Button handleClick={handleGoodClick} text='good' />
+      <Button handleClick={handleNeutralClick} text='neutral' />
+      <Button handleClick={handleBadClick} text='bad' />
+
+      <h2>Statistics</h2>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>All: {sum}</p>
+      <p>Average: {average}</p>
+      <p>Positive: {positive} %</p>
+   </div>
+  )
+}
+
+export default App
