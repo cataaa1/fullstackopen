47197e47980794ba8de14a0ef822661e190ceb38
cataaa1 <catalina@marquevich.com>
2026-09-17 16:23:24 -0300

    phonebook 3.3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -32,6 +32,18 @@ app.get('/api/info', (request, response) => {
     response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${Date()}`) 
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(p => p.id === id)
+
+    if (person){
+        response.json(person)
+    }
+    else{
+        response.status(404).end()
+    }
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
