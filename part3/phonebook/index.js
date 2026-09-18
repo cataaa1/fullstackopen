e2dbba9024b2e1e71145a008dbb58b328b443e92
cataaa1 <catalina@marquevich.com>
2026-09-18 11:59:00 -0300

    phonebook 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+
+morgan.token('body', (req) => {return req.method === 'POST' ? JSON.stringify(req.body) : ''})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
 
 let persons = [
     { 
@@ -82,6 +88,8 @@ app.post('/api/persons', (request,response) => {
     response.json(person)
   
 })
+
+
 
 const PORT = 3001
 app.listen(PORT)
